2ff432242d946bd20277f32758da0d6a2a12b327
Douglas Souza <douglas_search@hotmail.com>
2020-09-25 10:48:49 -0300

    improvements to the server.js file

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,14 +1,14 @@
-document.querySelector("#add - time")
-    .addEventListener('click', cloneField);
+document.querySelector("#add-time")
+    .addEventListener('click', cloneField)
 
 function cloneField() {
 
-    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
-    const fields = newFieldContainer.querySelectorAll('input');
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    const fields = newFieldContainer.querySelectorAll('input')
 
     fields.forEach(function (field) {
         field.value = ""
-    });
+    })
 
-    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const active = [
+const actives = [
     {
         name: "Douglas Souza",
         avatar: "https://avatars2.githubusercontent.com/u/50157211?s=60&v=4",
@@ -6,17 +6,6 @@ const active = [
         bio: "Passionate about animals and everything that involves the universe of pets.",
         subject: "Dog Walker",
         cost: "15",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Théo Henrique",
-        avatar: "https://avatars2.githubusercontent.com/u/50157211?s=60&v=4",
-        whatsapp: "99999999",
-        bio: "Passionate about animals and everything that involves the universe of pets.",
-        subject: "Dog Sitter",
-        cost: "50",
         weekday: [0],
         time_from: [720],
         time_to: [1220]
@@ -47,16 +36,18 @@ const weekdays = [
 
 function getSubject(subjectNumber) {
     const position = +subjectNumber - 1
-    return subjects (position)
+    return subjects[position]
 }
 
 function pageLanding(req, res) {
     return res.render("index.html")
 }
+
 function pageSearch(req, res) {
     const filters = req.query
-    return res.render("search.html", { active, filters, subjects, weekdays })
+    return res.render("search.html", { actives, filters, subjects, weekdays })
 }
+
 function pageWalkDogs(req, res) {
     const data = req.query
 
@@ -65,11 +56,10 @@ function pageWalkDogs(req, res) {
 
         data.subject = getSubject(data.subject)
 
-        active.push(data)
+        actives.push(data)
 
         return res.redirect("/search")
     }
-
 
     return res.render("care-dogs.html", { subjects, weekdays })
 }
