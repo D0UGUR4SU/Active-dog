1dafd00c3b0554fc881e482719735e546d4ff6ad
Douglas Souza <douglas_search@hotmail.com>
2020-09-30 13:30:12 -0300

    Creating database

---
+++ src/database/fake_data.js
@@ -0,0 +1,13 @@
+const professionals = [
+    {
+        name: "Douglas Souza",
+        avatar: "https://avatars2.githubusercontent.com/u/50157211?s=60&v=4",
+        whatsapp: "99999999",
+        bio: "Passionate about animals and everything that involves the universe of pets.",
+        subject: "Dog Walker",
+        cost: "15",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,71 +1,7 @@
-const actives = [
-    {
-        name: "Douglas Souza",
-        avatar: "https://avatars2.githubusercontent.com/u/50157211?s=60&v=4",
-        whatsapp: "99999999",
-        bio: "Passionate about animals and everything that involves the universe of pets.",
-        subject: "Dog Walker",
-        cost: "15",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Dog Beautician",
-    "Dog Host",
-    "Dog Photographer",
-    "Dog Sitter",
-    "Dog Walker",
-    "I work in NGOs for dogs",
-    "Pet shop",
-    "Trainer",
-    "Veterinary",
-]
-
-const weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageSearch(req, res) {
-    const filters = req.query
-    return res.render("search.html", { actives, filters, subjects, weekdays })
-}
-
-function pageWalkDogs(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        actives.push(data)
-
-        return res.redirect("/search")
-    }
-
-    return res.render("care-dogs.html", { subjects, weekdays })
-}
-
 const express = require('express')
 const server = express()
+
+const { pageLanding, pageSearch, pageCareDogs, saveForm} = require('./pages')
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
@@ -74,9 +10,11 @@ nunjucks.configure('src/views', {
 })
 
 server
+    .use(express.urlencoded({ extended: true}))
     .use(express.static("public"))
     .get("/", pageLanding)
     .get("/search", pageSearch)
-    .get("/care-dogs", pageWalkDogs)
+    .get("/care-dogs", pageCareDogs)
+    .post("/save-form", saveForm)
 
     .listen(5500)
